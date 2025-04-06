bb184446ed1bf4c92d31d89ad0f506c97a9133b1
Kibiwott <bk.kibiwott@gmail.com>
2025-04-06 09:33:28 +0300

    Initial project setup

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,15 @@
+ import {Container} from "@mui/material";
+ import Paper from '@mui/material/Paper';
+
+
+const LoginPage = () => {
+  return (
+    <Container maxWidth="xs">
+       <Paper elevation={0} />
+<Paper />
+<Paper elevation={3} />
+    </Container>
+  )
+}
+
+export default LoginPage
